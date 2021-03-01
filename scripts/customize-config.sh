b09e5c4b74b689bdcede8f8bf976d180fb960c7a
igithublab <66892031+igithublab@users.noreply.github.com>
2021-03-01 13:07:12 +0800

    set default WIFI SSID, encryption, key

--- scripts/customize-config.sh
+++ scripts/customize-config.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+WORKDIR=/workdir
+HOSTNAME=MT1300
+IPADDRESS=192.168.8.1
+SSID=MT1300
+ENCRYPTION=psk2+ccmp
+KEY=password
+
+cd "$WORKDIR/openwrt"
+
 # Modify default Hostname
-sed -i "s/hostname='OpenWrt'/hostname='MT1300'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='$HOSTNAME'/g" package/base-files/files/bin/config_generate
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/$IPADDRESS/g' package/base-files/files/bin/config_generate
 
 # Modify Timezone
 sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
@@ -15,8 +25,14 @@ sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/f
 sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/config_generate
 
-# Modify WiFi SSID
-sed -i 's/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid=MT1300/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify default WiFi SSID
+sed -i "s/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid='$SSID'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Encryption
+sed -i "s/set wireless.default_radio\${devidx}.encryption=none/set wireless.default_radio\${devidx}.encryption='$ENCRYPTION'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Key
+sed -i "/set wireless.default_radio\${devidx}.mode=ap/a\                        set wireless.default_radio\${devidx}.key='$KEY'" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Forced WiFi to enable
 sed -i 's/set wireless.radio\${devidx}.disabled=1/set wireless.radio\${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
