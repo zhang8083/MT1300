e1a8271cbf7182286d434d4796d5df782fd24d56
igithublab <66892031+igithublab@users.noreply.github.com>
2021-04-18 21:54:09 +0800

    Update customize-feeds-lienol.sh

--- scripts/customize-feeds-lienol.sh
+++ scripts/customize-feeds-lienol.sh
@@ -9,16 +9,18 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 ARCH=mipsle-hardfloat
 cd /workdir/openwrt
 mkdir -p files/etc/openclash/core
+cd files/etc/openclash/core
 
 clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 #clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 #clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
 
-wget -qO- $clash_main_url | gunzip -c > files/etc/openclash/core/clash
-#wget -qO- $clash_tun_url  | gunzip -c > files/etc/openclash/core/clash_tun
-#wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
+wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+#wget -qO- $clash_main_url | gunzip -c > clash
+#wget -qO- $clash_tun_url  | gunzip -c > clash_tun
+#wget -qO- $clash_game_url | tar xOvz > clash_game
 
-chmod +x files/etc/openclash/core/clash*
+chmod +x clash*
 
 
 mkdir /workdir/openwrt/package/lean/redsocks2 
