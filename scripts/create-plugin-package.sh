55536d16862eb646a4dc8209247152574ce19e4e
igithublab <66892031+igithublab@users.noreply.github.com>
2021-08-18 13:12:20 +0800

    fix installation bug

--- scripts/create-plugin-package.sh
+++ scripts/create-plugin-package.sh
@@ -2,7 +2,7 @@ cd openwrt/bin/targets/*/*
 mkdir plugin
 
 # plugin list in ../../../packages/mipsel_24kc
-pkglist="base/shadowsocksr-libev-*.ipk base/pdnsd-alt_*.ipk base/chinadns-ng_*.ipk base/microsocks_*.ipk base/dns2socks_*.ipk base/simple-obfs*.ipk base/tcping_*.ipk base/v2ray-plugin_*.ipk base/xray_*.ipk base/trojan_*.ipk base/ipt2socks_*.ipk base/redsocks2_*.ipk base/luci-*-passwall*.ipk base/luci-*-ssr-plus*.ipk luci/luci-compat_*.ipk base/xray-core_*.ipk gli_pub/shadowsocks-libev-ss-*.ipk base/shadowsocksr-libev-ssr-*.ipk base/luci-app-openclash*.ipk"
+pkglist="base/shadowsocksr-libev-*.ipk base/pdnsd-alt_*.ipk base/chinadns-ng_*.ipk base/microsocks_*.ipk base/dns2socks_*.ipk base/simple-obfs*.ipk base/tcping_*.ipk base/v2ray-plugin_*.ipk base/xray_*.ipk base/trojan*.ipk base/ipt2socks_*.ipk base/redsocks2_*.ipk base/luci-*-passwall*.ipk base/luci-*-ssr-plus*.ipk luci/luci-compat_*.ipk base/xray-core_*.ipk gli_pub/shadowsocks-libev-ss-*.ipk base/shadowsocksr-libev-ssr-*.ipk base/luci-app-openclash*.ipk"
 
 for pkg in $pkglist
 do
@@ -22,21 +22,20 @@ echo "Creating installation script for passwall"
 cat << EOF > ./plugin/install-passwall.sh
 opkg update
 opkg install luci luci-i18n-base-zh-cn ttyd luci-app-ttyd luci-i18n-ttyd-zh-cn luci-compat luci-lib-ipkg wget htop
-
-opkg install ./chinadns-ng_*.ipk 
+opkg install ./chinadns-ng_*.ipk
 opkg install ./dns2socks_*.ipk
 opkg install ./ipt2socks_*.ipk
-opkg install ./microsocks_*.ipk 
-opkg install ./pdnsd-alt_*.ipk 
-opkg install ./shadowsocks-libev-ss-local_*.ip
+opkg install ./microsocks_*.ipk
+opkg install ./pdnsd-alt_*.ipk
+opkg install ./shadowsocks-libev-ss-local_*.ipk
 opkg install ./shadowsocks-libev-ss-redir_*.ipk
-opkg install ./shadowsocksr-libev-ssr-local_*.ipk 
-opkg install ./shadowsocksr-libev-ssr-redir_*.ipk 
-opkg install ./simple-obfs*.ipk 
-opkg install ./tcping_*.ipk 
-opkg install ./trojan*.ipk 
+opkg install ./shadowsocksr-libev-ssr-local_*.ipk
+opkg install ./shadowsocksr-libev-ssr-redir_*.ipk
+opkg install ./simple-obfs*.ipk
+opkg install ./tcping_*.ipk
+opkg install ./trojan*.ipk
 opkg install ./xray-core_*.ip
-opkg install ./luci-app-passwall_*.ipk 
+opkg install ./luci-app-passwall_*.ipk
 opkg install ./luci-i18n-passwall-zh-cn_*.ipk
 EOF
 
@@ -46,29 +45,44 @@ echo "Creating installation script for ssr plus"
 cat << EOF > ./plugin/install-ssrp.sh
 opkg update
 opkg install luci luci-i18n-base-zh-cn ttyd luci-app-ttyd luci-i18n-ttyd-zh-cn luci-compat luci-lib-ipkg wget htop
-
 opkg install ./shadowsocksr-libev-alt_*.ipk
-opkg install ./pdnsd-alt_*.ipk 
-opkg install ./microsocks_*.ipk 
-opkg install ./dns2socks_*.ipk 
-opkg install ./shadowsocksr-libev-ssr-local_*.ipk 
+opkg install ./pdnsd-alt_*.ipk
+opkg install ./microsocks_*.ipk
+opkg install ./dns2socks_*.ipk
+opkg install ./shadowsocksr-libev-ssr-local_*.ipk
 opkg install ./simple-obfs*.ipk
-opkg install ./tcping_*.ipk 
-#opkg install ./v2ray-plugin_*.ipk 
-#opkg install ./xray_*.ipk 
-#opkg install ./trojan_*.ipk 
+opkg install ./tcping_*.ipk
+#opkg install ./v2ray-plugin_*.ipk
+#opkg install ./xray_*.ipk
+#opkg install ./trojan*.ipk
 opkg install ./ipt2socks_*.ipk
-#opkg install ./redsocks2_*.ipk 
+#opkg install ./redsocks2_*.ipk
 opkg install ./xray-core_*.ipk
 opkg install ./shadowsocks-libev-ss-local_*.ipk
-opkg install ./shadowsocks-libev-ss-server_*.ipk
 opkg install ./shadowsocks-libev-ss-redir_*.ipk
-opkg install ./shadowsocks-libev-ss-check_*.ipk
+opkg install ./shadowsocksr-libev-ss-redir_*.ipk
+opkg install ./shadowsocksr-libev-ss-check_*.ipk
 opkg install ./luci-app-ssr-plus_*.ipk
 opkg install ./luci-i18n-ssr-plus-zh-cn_*.ipk
 EOF
 
 chmod +x ./plugin/install-ssrp.sh
+
+echo "Creating installation script for openclash"
+cat << EOF > ./plugin/install-openclash.sh
+opkg update
+opkg install luci luci-i18n-base-zh-cn ttyd luci-app-ttyd luci-i18n-ttyd-zh-cn luci-compat luci-lib-ipkg wget htop
+opkg install ./redsocks2_*.ipk
+opkg install ./luci-app-openclash_*.ipk
+mkdir -p /etc/openclash/core
+cd /etc/openclash/core
+ARCH=mipsle-softfloat
+clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+chmod +x clash*
+EOF
+
+chmod +x ./plugin/install-openclash.sh
 
 
 tar czvf mt1300-plugin.tar.gz ./plugin
