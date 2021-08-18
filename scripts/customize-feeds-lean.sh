dc6d4e6c31f77469c7e2e760bd5f3c066e9055c9
igithublab <66892031+igithublab@users.noreply.github.com>
2021-08-18 17:13:23 +0800

    Update customize-feeds-lean.sh

--- scripts/customize-feeds-lean.sh
+++ scripts/customize-feeds-lean.sh
@@ -26,6 +26,26 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 #svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
 #svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
 
+cd /workdir/openwrt/package/lean
+# Add openclash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+ARCH=mipsle-softfloat
+cd /workdir/openwrt
+mkdir -p files/etc/openclash/core
+cd files/etc/openclash/core
+
+clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+
+wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+#wget -qO- $clash_main_url | gunzip -c > clash
+#wget -qO- $clash_tun_url  | gunzip -c > clash_tun
+#wget -qO- $clash_game_url | tar xOvz > clash_game
+
+chmod +x clash*
+
+
 # Fix mt76 wireless driver
 #cd  /workdir/openwrt/package/kernel/mt76
 #rm -f Makefile
